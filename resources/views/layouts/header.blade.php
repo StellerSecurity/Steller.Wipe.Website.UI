5dd0f14a8e6a1a6cf8200d86ebc73d389fbb8fd0
Your Name <you@example.com>
2023-12-18 23:41:27 +0100

    WipedBy

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <div class="navigation w-100 bg-grey-light d-flex align-items-center justify-content-between flex-wrap p-3">
   <div class="navbrand">
-    <a href="/" class="text-decoration-none">
+    <a href="https://stellarsecurity.com/?ref=protect"" class="text-decoration-none">
       <img src="/logo.svg" width="200">
     </a>
   </div>
